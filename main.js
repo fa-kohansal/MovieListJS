a0cdf49968f39eb8c26f9eca4c57deb58e21f96a
fa-kohansal <farnaz_kohansal@yahoo.com>
2026-05-20 13:23:18 -0700

    compelete project without responsive design

--- main.js
+++ main.js
@@ -1,74 +1,123 @@
-window.onload=()=>{
-    const movieList=[]
-    const movieListE1=document.getElementById("movieList");
-    const moviesBtn = document.getElementById("moviesBtn");
-    const showMovies = document.getElementById("listM");
-    const deletedList = document.getElementById("removalPart");
-    const deletedMovies = document.getElementById("deletedMovies");
-    let addMovieBtn= document.getElementById("addBtn");
-    let removalBtn = document.getElementById("removalBtn");
+const movieList = [];
 
-    const renderMovies = () => {
-      movieListE1.innerHTML = movieList.map((elem, index) => {
-        if(movieList[index].delete===false){
-           return `
-            <li data-index="${index}" class="list-group d-flex flex-row flex-nowrap col-12">
-                <button class="fave-btn btn btn-sm ${elem.favorite? "text-warning":"text-white"} mb-1 col-1 bi bi-star-fill"></button>
-                <p class="col-1 text-center mt-2 text-white">${elem.num}</p>
-                <p class="col-8 mt-2 text-white ">${elem.name}</p> 
-                <button class=" plus-btn btn btn-sm bi bi-check-circle col-1 text-white mb-1"></button> 
-                <button class=" delete-btn mb-1 btn btn-sm bi bi-trash-fill col-1 text-white"></button> 
+window.onload = () => {
+  const movieListE1 = document.getElementById("movieList");
+  const moviesBtn = document.getElementById("moviesBtn");
+  const showMovies = document.getElementById("listM");
+  const movieDetail = document.getElementById("movieDetils");
+  const deletedList = document.getElementById("removalPart");
+  const deletedMovies = document.getElementById("deletedMovies");
+  const numMovies = document.getElementById("countMovies");
+  const favoriteCount = document.getElementById("favoriteCount");
+  let addMovieBtn = document.getElementById("addBtn");
+  let removalBtn = document.getElementById("removalBtn");
+  let viewsMovie = 0;
+  const totalView = document.getElementById("totalView");
+  const deletedCount = document.getElementById("deletedCount");
+  const searchInput = document.getElementById("searchMovie");
+  searchInput.addEventListener("input", () => {
+    const filteredMovies = movieList.filter((movie) => {
+      return movie.name.includes(searchInput.value);
+    });
+    renderMovies(value ? filteredMovies: movieList);
+  });
+  const renderMovies = (movies) => {
+    movieListE1.innerHTML = movies.filter(m=>!m.deleted).map((elem) => {
+        
+          return `
+            <li data-id="${elem.id}" class="animate__animated animate__fadeIn list-group ${elem.view > 0 ? "watched " : ""} ${elem.favorite ? "border-danger" : " "} listStyle mb-1 d-flex flex-row flex-nowrap col-12">
+                <button class="fave-btn btn btn-sm ${elem.favorite ? "text-danger" : "movieActions"} mb-1 col-1 bi bi-heart-fill"></button>
+                
+                <p class="col-9 mt-2 text-light  text-capitalize">${elem.name}</p> 
+                <p class="plus-btn pointer-event col-1 text-center mt-2 bg ${elem.view > 0 ? "watchedIcon" : "movieActions"}"><i class="plus-btn bi bi-eye"></i> ${elem.view}</p>
+                
+                <button class="delete-btn mb-1 btn btn-sm bi bi-trash-fill col-1"></button> 
             </li>
-        `; 
-        }
-      }).join(" ");
-    };
-    addMovieBtn.addEventListener("click", () => {
-      let movieName = document.getElementById("addMovie").value;
-      movieList.push({ name: movieName, num: 0, favorite: false, delete:false });
-      document.getElementById("addMovie").value = "";
-      renderMovies();
+        `;
+        
+      })
+      .join("");
+    const activeMovie = movieList.filter((movie) => movie.deleted === false);
+    numMovies.innerHTML = activeMovie.length;
+    const faveNum = movieList.filter((movie) => movie.favorite === true);
+    favoriteCount.innerHTML = faveNum.length;
+    viewsMovie = movieList.reduce((total, elem) => {
+      return total + elem.view;
+    }, 0);
+    totalView.innerHTML = viewsMovie;
+    const delCount = movieList.filter((elem) => elem.deleted === true);
+    deletedCount.innerHTML = delCount.length;
+  };
+  addMovieBtn.addEventListener("click", () => {
+    let movieName = document.getElementById("addMovie").value;
+    movieList.push({
+      id: Date.now(),
+      name: movieName,
+      view: 0,
+      favorite: false,
+      deleted: false,
     });
-    movieListE1.addEventListener("click",(e)=>{
-        const li= e.target.closest("li");
-        if(!li) return;
-        const index= li.dataset.index;
-        if (e.target.classList.contains("fave-btn")){
-            movieList[index].favorite = !movieList[index].favorite
-            
-        }
-        if(e.target.classList.contains("plus-btn")){
-            movieList[index].num++;
-        }
-        if(e.target.classList.contains("delete-btn")){
-            movieList[index].delete=!movieList[index].delete
-            console.log(movieList);
-            
-        }
-      renderMovies();
+    document.getElementById("addMovie").value = "";
+    renderMovies(movieList);
+  });
+  movieListE1.addEventListener("click", (e) => {
+    const li = e.target.closest("li");
+    if (!li) return;
+    const id = Number(li.dataset.id);
+    const movie= movieList.find(m=>m.id===id)    
+    if(!movie) return;
+    if (e.target.classList.contains("fave-btn")) {
+      movie.favorite = !movie.favorite;
+    }
+    if (e.target.classList.contains("plus-btn")) {
+      movie.view++;
+    }
+    if (e.target.classList.contains("delete-btn")) {
+      movie.deleted = !movie.deleted;
+    }
+    renderMovies(movieList);
+  });
+  removalBtn.addEventListener("click", () => {
 
-    });
-    removalBtn.addEventListener("click",()=>{
-        showMovies.classList.add("d-none");
-        deletedList.classList.remove("d-none")
-        removalBtn.classList.add("d-none");
-        moviesBtn.classList.remove("d-none")
-        deletedMovies.innerHTML = movieList
-          .map((elem) => {
-            if (elem.delete === true) {
-              return `
+    movieDetail.classList.add("d-none");
+    movieDetail.classList.remove("d-flex");
+    deletedList.classList.remove("d-none");
+    // removalBtn.classList.add("d-none");
+    moviesBtn.classList.remove("d-none");
+    deletedMovies.innerHTML = movieList
+      .map((elem) => {
+        if (elem.deleted === true) {
+          return `
                     <li class="text-light">${elem.name}</li>
                 `;
-            }
-          })
-          .join(" ");    
-    });
-    moviesBtn.addEventListener("click",()=>{
-        showMovies.classList.remove("d-none");
-        deletedList.classList.add("d-none");
-        removalBtn.classList.remove("d-none");
-        moviesBtn.classList.add("d-none");
-    });
-    renderMovies()
-}
+        }
+      })
+      .join(" ");
+  });
+  moviesBtn.addEventListener("click", () => {
+    movieDetail.classList.remove("d-none");
+    movieDetail.classList.add("d-flex");
+    deletedList.classList.add("d-none");
+    removalBtn.classList.remove("d-none");
+    moviesBtn.classList.add("d-none");
+  });
+  renderMovies(movieList);
+  document.querySelectorAll(".dropdown-item").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const type = e.target.dataset.sort;
+
+      if (type === "last") {
+        movieList.sort((a, b) => b.id - a.id);
+      }
+      if (type === "most") {
+        movieList.sort((a, b) => b.view - a.view);
+      }
+      if (type === "less") {
+        movieList.sort((a, b) => a.view - b.view);
+      }
+      renderMovies(movieList);
+  });
+});
+
+};
 
